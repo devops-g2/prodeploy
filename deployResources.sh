b5bd3b22196e402376e15881130353aba08e7d6f
Kevin Sundberg <113990792+DueWeb@users.noreply.github.com>
2024-01-24 10:27:44 +0100

    Update deployResources.sh

--- deployResources.sh
+++ deployResources.sh
@@ -68,7 +68,7 @@ helm upgrade --install prometheus prometheus-community/kube-prometheus-stack --v
 --version 51.x \
 --create-namespace 
 bash grafanaScript.sh serve_from_sub_path=true
-bash grafanaScript.sh root_url=ip/grafana
+bash grafanaScript.sh root_url=mydomain/grafana/
 
 grafanaPod=$(kubectl get pods -n prometheus-grafana | awk '$1 ~ /^prometheus-grafana/ {print $1; exit}')
 
